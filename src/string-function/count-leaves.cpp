7b385f1c250444040227aef7d117856536818d51
Liviu Nicoara <nikkoara at hates dot ms>
2017-04-10 09:50:56 -0400

    Removed leftovers.

--- src/string-function/count-leaves.cpp
+++ src/string-function/count-leaves.cpp
@@ -12,11 +12,7 @@ using namespace std;
 
 #include <hacks/suffix-tree.hpp>
 
-using state_type = map< size_t, size_t >;
-
 int main (int, char** argv) {
-    cout << count_leaves (make_suffix_tree (argv [1]))
-         << endl;
-
+    cout << count_leaves (make_suffix_tree (argv [1])) << endl;
     return 0;
 }
